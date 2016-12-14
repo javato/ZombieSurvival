1a960dc964fb01979622a9e006d88d9586aab3b7
Javato <javirolga@gmail.com>
2016-12-14 04:41:34 +0200

    --- firebase --- - Data of Firebase Database linked to logged in users by firebase auth method - Now there are two record, individual one, and a world record - Translation to English in progress

---
+++ app/src/main/java/com/google/zxing/integration/android/User.java
@@ -0,0 +1,36 @@
+package com.google.zxing.integration.android;
+
+public class User {
+
+    private String name;
+    private int record;
+
+    public User() {
+        this.name = "Name doesn't established";
+        record = 0;
+    }
+
+    /*public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRecord(){
+        return record;
+    }
+
+    public void setRecord(){
+        this.record = record;
+    }
+}
